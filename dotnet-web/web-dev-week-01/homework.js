df3dbde38f7df505bb7c815703c427c54311640d
Will Eft <weft@my.wctc.edu>
2026-08-24 21:44:45 -0500

    Questions1+2

--- dotnet-web/web-dev-week-01/homework.js
+++ dotnet-web/web-dev-week-01/homework.js
@@ -4,3 +4,24 @@ const courses = [
   { code: "DAT200", title: "SQL Fundamentals", credits: 3, enrolled: 12, cap: 25 },
   { code: "PRG115", title: "Intro to C#", credits: 4, enrolled: 28, cap: 30 },
 ];
+
+// 1
+const courseLine = (course) => {
+  const { code, title, credits } = course;
+    return `${code}: ${title} (${credits} credits)`;
+}
+// 2
+const isFull = (course) => {
+  return course.enrolled === course.cap;
+}
+// 3
+
+// 4
+
+// 5 
+
+// 6 
+
+// 7
+
+// 8
